cd1e6e9b227f3e9d6b62eb6b1fbccc57c84d7a8b
unknown <gsmahato8@gmail.com>
2024-01-04 13:10:10 +0545

    second commit

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import FlatCards from "./components/FlatCards";
 import ElevatedCards from "./components/ElevatedCards";
 import FancyCards from "./components/FancyCards";
 import ActionCard from "./components/ActionCard";
+import ContactLists from "./components/ContactLists";
 
 function App(){
 return(
@@ -18,7 +19,8 @@ return(
       <FlatCards/>
       <ElevatedCards/>
       <FancyCards/>
-      <ActionCard/>
+      <ContactLists/>
+      <ActionCard/>  
     </View>
   </ScrollView>
 </SafeAreaView>
